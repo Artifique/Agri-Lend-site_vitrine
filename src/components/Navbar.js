2ac7116cab31d87120aa61d2fde8a65387ccc98c
Artifique <fombadaouda72@gmail.com>
2025-09-20 15:12:03 +0000

    troisieme commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,35 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../logo.png';
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <a href="/" className="navbar-logo">
+        <a href="/" className="navbar-logo" onClick={closeMobileMenu}>
           <img src={logo} alt="Agri-Lend Logo" style={{ height: '50px' }} />
         </a>
-        <ul className="nav-menu">
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
           <li className="nav-item">
-            <a href="#about" className="nav-links">
+            <a href="#about" className="nav-links" onClick={closeMobileMenu}>
               À propos
             </a>
           </li>
           <li className="nav-item">
-            <a href="#services" className="nav-links">
+            <a href="#services" className="nav-links" onClick={closeMobileMenu}>
               Services
             </a>
           </li>
           <li className="nav-item">
-            <a href="#contact" className="nav-links">
+            <a href="#contact" className="nav-links" onClick={closeMobileMenu}>
               Contact
             </a>
           </li>
